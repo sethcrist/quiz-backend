d0969c539d5757f95289e3673f33564467887df4
Seth Crist <sethcrist50@gmail.com>
2024-06-29 13:41:33 -0500

    first push

---
+++ src/main/java/com/example/ezquizbackend/EzQuizBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ezquizbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzQuizBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzQuizBackendApplication.class, args);
+    }
+
+}
